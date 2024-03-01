64373abb36fb618bbe883f523558820b7dd98021
chrisnastro <cnastro@icloud.com>
2024-02-29 21:02:23 -0500

    finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,16 +23,31 @@ res.status(200).json(category);
 }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(category);
+} catch (err) {
+  res.status(500).json({ message: 'Could not create, try again' });
+}
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+try {
+  const updated = await Category.update(req.body, { where: { id: req.params.id} });
+  !updated[0] ? res.status(404).json({ message: 'Category not found!'}) : res.status(200).json(updated);
+} catch (err) {
+  res.status(500).json({ message: 'Could not update, try again' });
+}
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+ try {
+  const deleted = await Category.destroy({ where: { id: req.params.id } });
+  !deleted ? res.status(404).json({ message: 'Category not found!'}) : res.status(200).json(deleted);
+ } catch (err) {
+  res.status(500).json(err);
+ }
 });
 
 module.exports = router;
