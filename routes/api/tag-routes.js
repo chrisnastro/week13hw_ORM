9476c8b1b2e0749c8295a5fae01134dad5e98a4e
chrisnastro <cnastro@icloud.com>
2024-03-04 13:15:04 -0500

    added update Tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,8 +36,17 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res) => {
+try {
+  const updatedTag = await Tag.update(req.body, {
+    where: { id: req.params.id },
+  });
+  !updatedTag[0]
+  ? res.status(404).json({ message: "ID does not have a Tag to update!"})
+  : res.status(200).json(updatedTag);
+} catch (err) {
+  res.status(500).json({ message: "Could not update Tag!"});
+}
 });
 
 router.delete('/:id', (req, res) => {
