2319045197a2c48661704c446ead7d4a6255a542
chrisnastro <cnastro@icloud.com>
2024-03-04 13:40:57 -0500

    fixed category route bug

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,7 +26,7 @@ res.status(200).json(category);
 router.post('/', async (req, res) => {
   try {
     const newCategory = await Category.create(req.body);
-    res.status(200).json(category);
+    res.status(200).json(newCategory);
 } catch (err) {
   res.status(500).json({ message: 'Could not create, try again' });
 }
