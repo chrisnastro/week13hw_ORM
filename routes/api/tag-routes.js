d435ddce55b77a9af81aa9788ad6afe94d61d47b
chrisnastro <cnastro@icloud.com>
2024-03-04 13:33:35 -0500

    reviewed all routes and models for proper syntax

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,8 +49,15 @@ try {
 }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+try {
+  const deletedTag = await Tag.destroy({ where: { id: req.params.id } });
+  !deletedTag
+  ? res.status(404).json({ message: "ID does not have a Tag!" })
+  : res.status(200).json(deletedTag);
+} catch (err) {
+  res.status(500).json({ message: "Could not delete Tag!"});
+}
 });
 
 module.exports = router;
